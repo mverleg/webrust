c0ffeec4cd4218c1866fc498ebe5166e6af40a23
Mark <mverleg.noreply@gmail.com>
2023-05-07 08:37:59 +0200

    Updating conf state

--- src/conf.rs
+++ src/conf.rs
@@ -8,33 +8,42 @@ use ::std::sync::LazyLock;
 
 use ::serde::Deserialize;
 use ::serde::Serialize;
-use ::tokio::sync::RwLock;
+use ::tokio::sync::Mutex;
 
-static CONF: LazyLock<Container<Conf>> = LazyLock::new(|| Container::<Conf> { conf: RwLock::new(None) });
+static CONF: LazyLock<ConfContainer> = LazyLock::new(ConfContainer::empty);
 
 #[derive(Debug)]
-pub struct Container<T> {
-    conf: RwLock<Option<(PathBuf, Arc<T>)>>,
+pub struct ConfContainer {
+    conf: Mutex<Option<(PathBuf, Arc<Conf>)>>,
 }
 
-impl <T: Default> Container<T> {
-    fn get(&mut self, pth: &Path) -> Arc<T> {
-        let mut val_ref = self.conf.get_mut();
-        match val_ref {
-            Some((old_pth, val)) => {
-                assert_eq!(pth, old_pth);
-                val.clone()
-            }
+impl ConfContainer {
+    pub fn empty() -> Self {
+        ConfContainer { conf: Mutex::new(None) }
+    }
+
+    fn get(&mut self, pth: &Path) -> Arc<Conf> {
+        let mut conf_ref = self.conf.get_mut();
+        match conf_ref {
+            Some((conf_pth, conf)) => {
+                assert_eq!(pth, conf_pth);
+                conf.clone()
+            },
             None => {
-                let new_val = Arc::new(Conf::load_from_disk(pth).unwrap_or_default());
-                *val_ref = Some((pth.to_owned(), new_val.clone()));
-                new_val
+                let new_conf = Arc::new(Conf::load_from_disk(pth).unwrap_or_default());
+                *conf_ref = Some((pth.to_owned(), new_conf.clone()));
+                new_conf
             }
         }
     }
 
-    fn set(&mut self, pth: &Path) -> Conf {
-        todo!()
+    fn set(&mut self, pth: &Path, new_conf: Conf) {
+        let new_state = Some((pth.to_owned(), Arc::new(new_conf)));
+        let mut conf_ref = self.conf.get_mut();
+        if let Some((conf_pth, _)) = conf_ref {
+            assert_eq!(pth, conf_pth);
+        }
+        *conf_ref = new_state
     }
 }
 
@@ -45,13 +54,11 @@ pub struct Conf {
 }
 
 impl Conf {
-    //TODO @mark: make these not public
     fn load_from_disk(pth: &Path) -> Option<Conf> {
         let reader = BufReader::new(File::options().write(false).open(pth).ok()?);
         serde_json::from_reader(reader).ok()
     }
 
-    //TODO @mark: make these not public
     fn save_to_disk(pth: &Path, conf: &Conf) {
         let writer = BufWriter::new(File::options().write(true).create(true).open(pth).unwrap());
         serde_json::to_writer_pretty(writer, conf).unwrap()
