c0ffee93adbd28a7a55e87a9bd302907214ce81d
Mark <mverleg.noreply@gmail.com>
2023-05-07 08:32:35 +0200

    Try to generify state loading

--- src/conf.rs
+++ src/conf.rs
@@ -10,22 +10,25 @@ use ::serde::Deserialize;
 use ::serde::Serialize;
 use ::tokio::sync::RwLock;
 
-static CONF: LazyLock<ConfContainer> = LazyLock::new(|| ConfContainer { conf: RwLock::new(None) });
+static CONF: LazyLock<Container<Conf>> = LazyLock::new(|| Container::<Conf> { conf: RwLock::new(None) });
 
 #[derive(Debug)]
-pub struct ConfContainer {
-    conf: RwLock<Option<(PathBuf, Arc<Conf>)>>,
+pub struct Container<T> {
+    conf: RwLock<Option<(PathBuf, Arc<T>)>>,
 }
 
-impl ConfContainer {
-    fn get(&mut self, pth: &Path) -> Arc<Conf> {
-        let mut conf_ref = self.conf.get_mut();
-        match conf_ref {
-            Some((_, conf)) => conf.clone(),
+impl <T: Default> Container<T> {
+    fn get(&mut self, pth: &Path) -> Arc<T> {
+        let mut val_ref = self.conf.get_mut();
+        match val_ref {
+            Some((old_pth, val)) => {
+                assert_eq!(pth, old_pth);
+                val.clone()
+            }
             None => {
-                let new_conf = Arc::new(Conf::load_from_disk(pth).unwrap_or_default());
-                *conf_ref = Some((pth.to_owned(), new_conf.clone()));
-                new_conf
+                let new_val = Arc::new(Conf::load_from_disk(pth).unwrap_or_default());
+                *val_ref = Some((pth.to_owned(), new_val.clone()));
+                new_val
             }
         }
     }
@@ -42,11 +45,13 @@ pub struct Conf {
 }
 
 impl Conf {
+    //TODO @mark: make these not public
     fn load_from_disk(pth: &Path) -> Option<Conf> {
         let reader = BufReader::new(File::options().write(false).open(pth).ok()?);
         serde_json::from_reader(reader).ok()
     }
 
+    //TODO @mark: make these not public
     fn save_to_disk(pth: &Path, conf: &Conf) {
         let writer = BufWriter::new(File::options().write(true).create(true).open(pth).unwrap());
         serde_json::to_writer_pretty(writer, conf).unwrap()
