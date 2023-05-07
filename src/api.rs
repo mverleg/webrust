c0ffee3fd691e055b26f14e97585a4edfb45a3ab
Mark <mverleg.noreply@gmail.com>
2023-05-07 20:08:47 +0200

    Update configuration

--- src/api.rs
+++ src/api.rs
@@ -1,15 +1,16 @@
 use ::std::path::PathBuf;
 use std::sync::Arc;
 
-use ::axum::extract::State;
+use ::axum;
+use ::axum::extract;
 use ::axum::http::Method;
 use ::axum::http::StatusCode;
 use ::axum::Json;
 use ::clap::Arg;
 use ::serde::Serialize;
-use ::axum;
-use crate::AppState;
+use ::tracing::info;
 
+use crate::AppState;
 use crate::args::Args;
 use crate::conf::Conf;
 
@@ -39,23 +40,32 @@ pub async fn api_index() -> Json<ApiIndex> {
     })
 }
 
-pub async fn api_conf_get(State(state): State<AppState>) -> (StatusCode, Json<Status<Conf>>) {
+pub async fn api_conf_get(extract::State(state): extract::State<AppState>) -> (StatusCode, Json<Status<Conf>>) {
     (
         StatusCode::OK,
         Json(Status {
             is_ok: true,
             msg: "latest config".to_string(),
             data: Some((*state.conf()).clone()),
-            //TODO @mark: get this as argument
         })
     )
 }
 
-pub async fn api_conf_put(State(args): State<Arc<Args>>) -> (StatusCode, Json<Vec<u8>>) {
-    //TODO @mark: I didn't find an automatic way to do this...
-    unimplemented!()
+// curl -XPUT -H "Content-Type: application/json" http://localhost:8080/api/conf -d '{"name":"Hello world!","score":37}'
+// curl -XGET curl http://localhost:8080/api/conf
+pub async fn api_conf_put(extract::State(state): extract::State<AppState>, extract::Json(conf): extract::Json<Conf>) -> (StatusCode, Json<Status<Conf>>) {
+    info!("updating conf to {:?}", &conf);
+    state.conf_container.set(&state.args.conf_state_path, conf.clone());
+    (
+        StatusCode::OK,
+        Json(Status {
+            is_ok: true,
+            msg: "latest config".to_string(),
+            data: Some(conf),
+        })
+    )
 }
 
-pub async fn api_conf_patch(State(args): State<Arc<Args>>) -> (StatusCode, Json<Vec<u8>>) {
+pub async fn api_conf_patch(extract::State(args): extract::State<AppState>) -> (StatusCode, Json<Status<Conf>>) {
     unimplemented!()
 }
